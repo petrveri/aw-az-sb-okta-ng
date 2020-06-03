c1217e76c3b90daf66499a93323fc51d8614ec96
Petro Veriienko <petrveri@gmail.com>
2020-06-03 14:30:15 -0400

    added CORS support added Pagination

--- src/main/java/org/petrveri/aw/controller/saleslt/CustomerWithAddressController.java
+++ src/main/java/org/petrveri/aw/controller/saleslt/CustomerWithAddressController.java
@@ -6,11 +6,14 @@ import org.petrveri.aw.repository.saleslt.CustomerWithAddressRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -26,15 +29,18 @@ public class CustomerWithAddressController {
     private CustomerWithAddressRepository customerWithAddressRepository;
 
     @GetMapping("")
-    Iterable<CustomerWithAddressDTO> getCustomers() {
+    @CrossOrigin
+    Iterable<CustomerWithAddressDTO> getCustomers(/*@RequestParam(required = false, defaultValue = "0") int page,
+                                    @RequestParam(required = false, defaultValue = "20") int size*/) {
         log.info("Request to retrieve all customers with addresses");
-        Iterable<CustomerWithAddress> customers = customerWithAddressRepository.findAll();
+        Iterable<CustomerWithAddress> customers = customerWithAddressRepository.findAll(/*PageRequest.of(page, size)*/);
         return StreamSupport.stream(customers.spliterator(), false)
                 .map(this::convertToDto)
                 .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
+    @CrossOrigin
     ResponseEntity<?> getCustomerWithAddressById(@PathVariable Integer id) {
         log.info("Request to retrieve a customer with addresses by id");
         Optional<CustomerWithAddress> optCustomerWA = customerWithAddressRepository.findById(id);
